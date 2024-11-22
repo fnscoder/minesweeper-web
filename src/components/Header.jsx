b9429e7c7ad6fb37c503b0a0b19710ef3ce27c77
Felipe Souza <fnscoder@gmail.com>
2024-11-22 09:35:30 -0300

    Updated API url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="bg-gray-800 text-white p-4 items-center flex flex-col">
       <h3 className="text-3xl font-bold">Minesweeper</h3>
-      <nav className="flex gap-10 justify-center">
+      <nav className="flex gap-5 justify-center">
         <Link to="/" className="mx-2 hover:underline">Home</Link>
         <Link to="/leaderboard" className="hover:underline">Leaderboard</Link>
         <a href="https://en.wikipedia.org/wiki/Minesweeper_(video_game)" target="_blank" rel="noreferrer" className="mx-2 hover:underline">About</a>
